0a4b7e39f5f548dd04bff94c7d3037351557e510
Chinwei Wong <chinweiwong@Chinweis-MacBook-Pro.local>
2017-01-07 09:32:09 -0800

    - clicking on dots now work

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,13 +1,27 @@
-$('.pagination svg').each(function(i, classname){
-	var targetID = $(this).attr('data-target')
-	var target = $('#'+targetID);
+$('.pagination .scroll-link').each(function(i, classname){
+	var targetID = $(this).attr('href')
 	var $this = this
 
 	new Waypoint({
-		element: target,
+		element: $(targetID),
 		handler: function(direction) {
-		$($this).siblings().removeClass('is-selected')
-			$($this).addClass('is-selected')
-		}
+		$($this).siblings().children('svg').removeClass('is-selected')
+			$($this).children('svg').addClass('is-selected')
+		},
+		offset: 30
 	})
+})
+
+
+$('a.scroll-link').on('click', function(){
+	var target = $(this).attr('href')
+
+	if (target == "intro") {
+		TweenLite.to(window, 1, {scrollTo: 0});
+	} else {
+		TweenLite.to(window, 1, {scrollTo: $(target).position().top});	
+	}	
+
+	return false;
+	
 })
