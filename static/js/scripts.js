1e1a2e5989a5b65c3326327aecdf2fe1d5519bcc
Chinwei Wong <chinweiwong@Chinweis-MacBook-Pro.local>
2017-01-07 14:34:08 -0800

    - removed hover states for mobile devices - tweaked styling for mobile

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,3 +1,27 @@
+// Remove hover states for touch devices
+
+var touch = 'ontouchstart' in document.documentElement
+            || (navigator.MaxTouchPoints > 0)
+            || (navigator.msMaxTouchPoints > 0);
+
+if (touch) { // remove all :hover stylesheets
+    try { // prevent exception on browsers not supporting DOM styleSheets properly
+        for (var si in document.styleSheets) {
+            var styleSheet = document.styleSheets[si];
+            if (!styleSheet.rules) continue;
+
+            for (var ri = styleSheet.rules.length - 1; ri >= 0; ri--) {
+                if (!styleSheet.rules[ri].selectorText) continue;
+
+                if (styleSheet.rules[ri].selectorText.match(':hover')) {
+                    styleSheet.deleteRule(ri);
+                }
+            }
+        }
+    } catch (ex) {}
+} 
+
+
 $('.pagination .scroll-link').each(function(i, classname){
 	var targetID = $(this).attr('href')
 	var $this = this
@@ -7,8 +31,7 @@ $('.pagination .scroll-link').each(function(i, classname){
 		handler: function(direction) {
 		$($this).siblings().children('svg').removeClass('is-selected')
 			$($this).children('svg').addClass('is-selected')
-		},
-		offset: 30
+		}
 	})
 })
 
